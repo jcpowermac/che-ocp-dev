bb5255a8e1beb613dc14663138654159b800abc8
Joseph Callen <jcallen@redhat.com>
2020-09-28 13:53:53 -0400

    add jq

--- install-editor-tooling.sh
+++ install-editor-tooling.sh
@@ -10,7 +10,7 @@
 
 set -e
 
-TOOLING=(vim tmux zsh curl zip wget bash-completion)
+TOOLING=(vim tmux zsh curl zip wget bash-completion jq)
 
 if command -v dnf 2> /dev/null; then
   dnf install -y "${TOOLING[@]}"
